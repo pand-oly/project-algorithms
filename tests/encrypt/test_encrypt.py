df5c8ac34a2ddb03c37a33547da106ead3adac9f
pand-oly <hoa98hoa@gmail.com>
2023-01-26 16:52:16 -0300

    feat(Requirement 2): test function encrypt_mensage

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,24 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from pytest import raises
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    string = "srt"
+    number = 123
+
+    with raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(number, number)
+
+    with raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(string, string)
+
+    message = "52050"
+
+    key_case2 = 3
+    assert encrypt_message(message, key_case2) == "025_05"
+
+    key_case1 = 4
+    assert encrypt_message(message, key_case1) == "0_5025"
+
+    key_case3 = -1
+    assert encrypt_message(message, key_case3) == "05025"
