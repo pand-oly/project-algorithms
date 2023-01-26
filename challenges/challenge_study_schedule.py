c90b9880edb33d7cdcc4138fed1ed68bfec8a974
pand-oly <hoa98hoa@gmail.com>
2023-01-25 21:28:13 -0300

    feat(Requirement 1): function that recive list of tuples with entry and exit times of students and target_time type int and return the number of students present at target_time

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,16 @@
-def study_schedule(permanence_period, target_time):
+from typing import List, Tuple, Union
+
+
+def study_schedule(permanence_period: List[Tuple], target_time: int) -> Union[int, None]:
     """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    count = 0
+    for n1, n2 in permanence_period:
+        if (type(n1) is not int) or (type(n2) is not int):
+            return None
+        elif n1 <= target_time <= n2:
+            count += 1
+
+    return count
