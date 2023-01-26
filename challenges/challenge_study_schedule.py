93e917be3c373cf51ef21ec6ecae06a52ca8b6b8
pand-oly <hoa98hoa@gmail.com>
2023-01-26 18:53:58 -0300

    fix(challenge study schedule linter): fix lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,16 @@
 from typing import List, Tuple, Union
 
 
-def study_schedule(permanence_period: List[Tuple], target_time: int) -> Union[int, None]:
+def study_schedule(
+    permanence_period: List[Tuple], target_time: int
+) -> Union[int, None]:
     """Faça o código aqui."""
     if target_time is None:
         return None
 
     count = 0
     for n1, n2 in permanence_period:
-        if (type(n1) is not int) or (type(n2) is not int):
+        if type(n1) is not int or type(n2) is not int:
             return None
         elif n1 <= target_time <= n2:
             count += 1
