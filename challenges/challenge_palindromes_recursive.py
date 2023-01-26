bd862876cb7470dba70391d8c1132c13f004182b
pand-oly <hoa98hoa@gmail.com>
2023-01-26 18:52:01 -0300

    feat(Requirement 3): is palindrome recursive function

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
-def is_palindrome_recursive(word, low_index, high_index):
+def is_palindrome_recursive(word, low_index, high_index) -> bool:
     """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0 or word[low_index] != word[high_index]:
+        return False
+    elif low_index >= high_index:
+        return True
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
