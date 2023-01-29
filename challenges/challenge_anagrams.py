1163339a7488599326c153ef3a076365c5a384ff
pand-oly <hoa98hoa@gmail.com>
2023-01-29 12:20:36 -0300

    feat(Requirement 4):Check if both strings are anagrams, use method quick sort to sort the two strings

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,48 @@
-def is_anagram(first_string, second_string):
+from typing import Tuple, List
+
+
+def quick_sort(array_strings: List[str], start: int, end: int) -> None:
+    if start < end:
+        p = partition(array_strings, start, end)
+        quick_sort(array_strings, start, p - 1)
+        quick_sort(array_strings, p + 1, end)
+
+
+def partition(array_strings: List[str], start: int, end: int) -> int:
+    pivot = array_strings[end]
+    delimiter = start - 1
+
+    for i in range(start, end):
+        if array_strings[i] <= pivot:
+            delimiter = delimiter + 1
+            array_strings[i], array_strings[delimiter] = (
+                array_strings[delimiter],
+                array_strings[i],
+            )
+
+    array_strings[delimiter + 1], array_strings[end] = (
+        array_strings[end],
+        array_strings[delimiter + 1],
+    )
+
+    return delimiter + 1
+
+
+def is_anagram(first_string: str, second_string: str) -> Tuple[str, str, bool]:
     """Faça o código aqui."""
-    raise NotImplementedError
+    list_string = list(first_string.lower())
+    quick_sort(list_string, 0, len(list_string) - 1)
+    order_first_string = "".join(list_string)
+
+    list_string = list(second_string.lower())
+    quick_sort(list_string, 0, len(list_string) - 1)
+    order_second_string = "".join(list_string)
+
+    if first_string == "" or second_string == "":
+        return order_first_string, order_second_string, False
+
+    return (
+        order_first_string,
+        order_second_string,
+        (order_first_string == order_second_string),
+    )
