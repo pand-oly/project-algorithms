fb7f4b8688fa9b4a8a0c64e88ace0f36fb8e345b
pand-oly <hoa98hoa@gmail.com>
2023-01-29 16:19:54 -0300

    feat(Requirement 5):

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,17 @@
-def find_duplicate(nums):
+from typing import List, Union
+
+
+def find_duplicate(nums: List[int]) -> Union[int, bool]:
     """Faça o código aqui."""
-    raise NotImplementedError
+    if nums is None or len(nums) < 2:
+        return False
+
+    nums.sort()
+    for i in range(len(nums) - 1):
+        if type(nums[i]) is not int or nums[i] <= 0:
+            return False
+
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+
+    return False
