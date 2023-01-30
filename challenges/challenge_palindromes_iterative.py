93df6a60e38a2a21b8df6241e41ab4fa1d457251
pand-oly <hoa98hoa@gmail.com>
2023-01-30 13:55:07 -0300

    feat(Requirement 6):

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,22 @@
-def is_palindrome_iterative(word):
+def is_palindrome_iterative(word: str) -> bool:
     """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0 or word is None:
+        return False
+
+    start = 0
+    end = len(word) - 1
+    result = True
+
+    while start != end:
+        if word[start] == word[end]:
+            result = True
+        else:
+            return False
+
+        start += 1
+        end -= 1
+
+        if start >= end:
+            break
+
+    return result
